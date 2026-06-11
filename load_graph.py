33b1f8e3c8d03a351745324d47da5c318f540494
flipixcool <danilasavcenko5@gmail.com>
2026-06-11 16:15:44 +0300

    feat: add all from proj

---
+++ load_graph.py
@@ -0,0 +1,36 @@
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+import os
+# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+
+load_dotenv()
+
+
+URI = os.getenv("NEO4J_URI")
+AUTH = (os.getenv("NEO4J_USER"), os.getenv("NEO4J_PASSWORD"))
+
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    driver.verify_connectivity()
+    print("Connected to Neo4j")
+
+
+def load_material(driver, material: dict) -> None:
+    driver.execute_query(
+        """
+        MERGE (m:Material {material_id: $material_id})
+        SET m.name = $name
+        """,
+        material_id=material["material_id"],
+        name=material["name"],
+    )
+
+
+material = {
+    "material_id": "mat_2",
+    "name": "Copper Alloy",
+}
+
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    driver.verify_connectivity()
+    load_material(driver, material)
+    print("Material loaded")
